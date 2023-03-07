9206ff4b0dbdee6f01272cda6879260b4f1e8289
Gabriel Frasson <bielfc27@gmail.com>
2023-03-07 13:50:52 -0300

    Create task form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { PlusCircle } from  'phosphor-react';
+
 import { Header } from './components/Header';
+
+import styles from './App.module.css';
+
 import './global.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <div className={styles.wrapper}>
+        <form className={styles.creteTaskForm}>
+          <input
+            id="task"
+            name="task"
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+          />
+          <button type="submit">
+            Criar
+            <PlusCircle size={16} />
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
